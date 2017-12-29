4c20a8beb61de4b8ad1af3c90cdcf39b58fa0465
Frederik Tilmann <ftilmann@zedat.fu-berlin.de>
2017-12-29 18:17:02 +0100

    --flatten: if file is not found, do not fail, simply warn and leave command unexpanded -- inspired by issue #112

---
+++ testsuite/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{
+char string[30];
+while(1){
+printf("enter:");
+scanf("%[+-01-9]",string);
+while(getchar()!=10);
+printf("...%s..%d..\n",string,atoi(string));
+string[0]='\0';
+}
+}
